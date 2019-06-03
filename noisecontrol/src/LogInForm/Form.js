f6409a89947b2c1120269b25ac245b74a400ac1d
Your Name <aujones95@gmail.com>
2019-06-03 17:10:29 -0400

    partially done with the form

--- noisecontrol/src/LogInForm/Form.js
+++ noisecontrol/src/LogInForm/Form.js
@@ -22,6 +22,27 @@ class Form extends React.Component {
     }
 
     submitHandler = e => {
-        this.props.add
+        this.props.addClass(e, this.state.class);
+
+        this.setState({
+            class: {
+                name:'',
+                number:''
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <form id='form' onSubmit={this.submitHandler}>
+                    <input onChange={this.changeHandler} type='text' id = 'name' placeholder='Teachers Name' name='name' value={this.state.class.name}/>
+                    <input onChange={this.changeHandler} type='text' id = 'number' placeholder='Number of Students' name='number' value={this.state.class.number}/>
+                    <button onChange={this.changeHandler} type='submit'>New Class</button>                
+                </form>
+            </div>
+        )
     }
 } 
+
+export default Form;
