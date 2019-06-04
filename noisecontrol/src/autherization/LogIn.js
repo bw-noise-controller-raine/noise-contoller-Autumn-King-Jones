822cb06fbfb05e2f640127b65c56e30c3d1200e9
Your Name <aujones95@gmail.com>
2019-06-04 15:08:08 -0400

    made a login.js

---
+++ noisecontrol/src/autherization/LogIn.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Loader from 'react-loader-spinner';
+import { login } from '../actions'
+
+class LogIn extends React.Component {
+    state = {
+        credentials: {
+            username: '',
+            password: ''
+        }
+    };
+
+    changeHandler = e => {
+        this.setState({
+            credentials: {
+                ...this.state.credentials,
+                [e.target.name]: e.target.value
+            }
+        })
+    };
+
+    login = e => {
+        e.preventDefault();
+        this.props.login(this.state.credentials).then(() => {
+            this.props.history.push('/protected');
+        })
+    };
+
+    render() {
+        return (
+            <form onSubmit={this.login}>
+                <input
+                type = 'text'
+                name = 'username'
+                value = {this.state.credentials.username}
+                onChange = {this.changeHandler}
+                />
+
+                <input
+                 type = 'password'
+                 name = 'password'
+                 value = {this.state.credentials.password}
+                 onChange = {this.changeHandler}
+                 />
+            <button>
+                {this.props.isLoggedIn ? (
+                    <Loader type = 'ThreeDots' color = 'mediumpurple' height = '10' width = '20' />
+                ):(
+                    'Log In'
+                )}
+                </button> 
+            </form>
+        )
+    }
+}
+
+const mapStateToProps = state => ({
+    isLoggedIn: state.isLoggedIn
+});
+
+export default connect(
+    mapStateToProps,
+    {login}
+)(LogIn)
