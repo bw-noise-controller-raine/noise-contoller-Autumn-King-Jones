217cfca04a33b4ea4d4c5032543b82f887b6a8bb
Your Name <aujones95@gmail.com>
2019-06-04 10:49:58 -0400

    working in actions

---
+++ noisecontrol/src/actions/index.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+
+export const LOGIN = 'LOGIN'
+export const SUCCESS = 'SUCCESS'
+export const FAILURE = 'FAILURE'
+
+export const login = creds => dispatch => {
+    dispatch({ type:LOGIN});
+    return axios 
+    .post('https://noise-controller.herokuapp.com/api/auth/login', creds)
+    .then(res => {
+        localStorage.setItem('token', res.data.payload);
+        dispatch({ type: SUCCESS, payload: res.data.payload})
+    })
+}
+
+export const FETCHING_CLASSES = 'FETCHING_CLASSES';
+export const FETCHING_SUCCESS = 'FETCHING_SUCCESS';
+export const FETCHING_FAILURE = 'FETCHING_FAILURE';
+
+export const getclasses = () => dispatch => {
+    dispatch({ type: FETCHING_CLASSES});
+    axios
+    .get('https://noise-controller.herokuapp.com/api/auth')
+    .then(res => {
+        dispatch({ type: FETCHING_SUCCESS, payload: res.data})
+    })
+    .catch (error => {
+        console.log(error)
+    })
+}
