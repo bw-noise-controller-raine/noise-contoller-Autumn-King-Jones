e13ce6df173a9aa4afb51d0f29cf860b38d20738
Your Name <aujones95@gmail.com>
2019-06-06 11:16:50 -0400

    working on the css

--- noisecontrol/src/authorization/LogIn.js
+++ noisecontrol/src/authorization/LogIn.js
@@ -34,32 +34,39 @@ class LogIn extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className='container'>
+            <div className='Form' >
                 <form onSubmit={this.login}>
-                    <input
+                    
+                    <input className='formInput'
                         type='text'
                         name='username'
                         value={this.state.credentials.username}
                         onChange={this.changeHandler}
+                        placeholder='UserName'
                     />
+                    <br/>
 
-                    <input
+                    <input className='formInput'
                         type='password'
                         name='password'
                         value={this.state.credentials.password}
                         onChange={this.changeHandler}
+                        placeholder='Password'
                     />
-                    
-                    <button type='submit'>
+                     <br/>
+                    <button className='LogButton' type='submit'>
                         {this.props.isLoggedIn ? (
                             <Loader type='ThreeDots' color='mediumpurple' height='10' width='20' />
                         ) : (
                                 'Log In'
                             )}
                     </button>
-                    
+                   
+                    <button className='RegButton' onClick={this.registerHandler}>Register</button>
                 </form>
-                <button onClick={this.registerHandler}>Register</button>
+               
+            </div>
             </div>
         )
     }
